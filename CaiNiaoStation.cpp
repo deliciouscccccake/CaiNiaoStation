a91f1215f3940fce72331425ab667b6cbc90f3c4
CCCCCake <1215036436@qq.com>
2025-07-13 23:19:26 +0800

    task5 implemented

--- CaiNiaoStation.cpp
+++ CaiNiaoStation.cpp
@@ -30,13 +30,14 @@ struct Edge
         bool operator()(const Edge& a, const Edge& b) {
             return a.d > b.d;
         }
-};
+	};
 };
 bool MAP::InsertLocation(const string &target) {
 	auto it = Loclist.find(target);
 	if (it == Loclist.end())
 	{
-		Loclist[target]=Loclist.size();
+		int id = Loclist.size();
+		Loclist[target] = id + 1;
 		Roads.push_back(std::vector<EdgeNode>());
 		return true;
 	}
@@ -49,8 +50,8 @@ bool MAP::InsertRoad(int id1, int id2, int distance) {
 }
 
 vector<long long> MAP::Dijkstra(int start){	
-	vector<int> visited(Loclist.size(), 0);//记录遍历过的节点
-	vector<long long> dist(Loclist.size(), INT_MAX);//记录到起点的最短路径长
+	vector<int> visited(Loclist.size() + 1, 0);//记录遍历过的节点
+	vector<long long> dist(Loclist.size() + 1, LONG_LONG_MAX);//记录到起点的最短路径长
 	//vector<int> path(Loclist.size(), 0);//记录上一个节点，暂时不用
 	priority_queue<Edge, vector<Edge>, Edge::cmp> prqueues;//存入可用的dist
 
@@ -65,7 +66,7 @@ vector<long long> MAP::Dijkstra(int start){
         visited[cur.v] = true;
         for(const auto &ptr:Roads[cur.v])
         {
-            if(dist[cur.v] + ptr.distance < dist[ptr.to])
+            if(dist[cur.v] + ptr.distance < dist[ptr.to]) // 不需要visited判断，因为遍历过的一定存的是最短路径（Prim需要）
             {
                 dist[ptr.to] = dist[cur.v] + ptr.distance;
                 prqueues.push({ptr.to, dist[ptr.to]});
@@ -73,6 +74,52 @@ vector<long long> MAP::Dijkstra(int start){
         }
     }
 	return dist;
+}
+
+std::pair<std::vector<int>, std::vector<long long>> MAP::Prim(int s=1)
+{
+	vector<int> visited(Loclist.size() + 1, 0); // 记录遍历过的节点
+	vector<long long> dist(Loclist.size() + 1, LONG_LONG_MAX); //记录能拓展到该节点的最短边
+	vector<int> pre(Loclist.size() + 1, 0);//记录父节点
+	priority_queue<Edge, vector<Edge>, Edge::cmp> prqueues;//存入可用的dist // 没有用手写的优先队列，这又何尝不是一种背叛（）
+
+    //初始化
+    dist[s] = 0;
+	prqueues.push({s, 0});
+	pre[s] = s;
+    while(!prqueues.empty())
+    {
+        auto cur = prqueues.top();
+        prqueues.pop();
+        if(visited[cur.v])continue;
+        visited[cur.v] = true;
+        for(const auto &ptr:Roads[cur.v])
+        {
+            if(!visited[ptr.to] && ptr.distance < dist[ptr.to])
+            {
+                dist[ptr.to] = ptr.distance;
+				pre[ptr.to] = cur.v;
+                prqueues.push({ptr.to, dist[ptr.to]});
+            }
+        }
+    }
+	return {pre, dist};
+}
+
+int Tree::init_dfs(int p)
+{
+	if(fa[p][0] == p)return dep[p] = 0;
+	else if(dep[p])return dep[p]; // 只有根结点和没被遍历过的结点的深度才是0
+	else return dep[p] = init_dfs(fa[p][0]) + 1;
+}
+
+Tree::~Tree()
+{
+	for(int i = 1;i <= n;++i)
+	{
+		delete[] fa[i], maxd[i];
+	}
+	delete[] fa, maxd;
 }
 
 void Init(const string& command)
@@ -105,4 +152,38 @@ string task3(const string& command) {
 	for (int i = 0; i < packnum; i++)
 		ss >> packages[i].id >> packages[i].weight >> packages[i].dest >> packages[i].Stime >> packages[i].Ttime;
 
+}
+
+string task5(const string &command) // 根据样例给的输入，函数内还需建一个新图
+{
+	MAP g;//覆盖全局变量g
+	int n, m, q;
+	string outStr = "";
+	std::istringstream iss(command);
+
+	iss >> n >> m;
+	for(int i = 1;i <= n;++i)
+		g.InsertLocation(std::to_string(i)); // 单纯是为了配合动态建图的过程，不想再新写一个没有地点名称的建图了
+	for(int i = 1;i <= m;++i)
+	{
+		int u, v, w;
+		iss >> u >> v >> w;
+		g.InsertRoad(u, v, w);
+	}
+	auto mSTRes = g.Prim(); // 最小生成树的结果
+	Tree mST = Tree(mSTRes.first, mSTRes.second);
+	iss >> q;
+	for(int i = 1;i <= q;++i)
+	{
+		int x, y;
+		iss >> x >> y;
+		outStr += to_string(mST.MaxdOnLCA(x, y)) + '\n';
+	}
+	return outStr;
+}
+
+int main() // 只是为了方便测试，到时迁移到Qt时需要删去
+{
+	cout << task5("4 4\n1 2 4\n2 3 3\n3 1 1\n1 4 2\n3\n3 4\n1 2\n4 2\n");
+	return 0;
 }
--- CaiNiaoStation.h
+++ CaiNiaoStation.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <algorithm>
 #include <string>
 #include <vector>
 #include <map>
@@ -20,6 +21,7 @@ public:
 	bool InsertLocation(const string &target);//添加元素
 	bool InsertRoad(int id1, int id2, int distance);//添加id1和id2之间的双向边
 	std::vector<long long> Dijkstra(int start);//计算单个点到各个点的最小距离
+	std::pair<std::vector<int>, std::vector<long long>> Prim(int s);//从s开始计算最小生成树，返回两个vector，分别表示最小生成树上：结点的双亲编号，结点的双亲边的长度
 }g;
 struct Package {
 	int id;
@@ -34,10 +36,74 @@ struct trolley {//快递小车
 	int maxlweight;//最大装载重量
 };
 
+class Tree
+{
+private:
+	int **fa, n, lgn, *dep; // 存储双亲以及多层双亲，需确保根节点的双亲是自己，第一维是结点编号，第二维是双亲层数(fa[][j]是第2^j个双亲)；结点数；(log_2)结点数 取上界；结点的深度
+	long long **maxd; // maxd[][j]是到第2^j个双亲的路径上的最长边
+	// 没有保存根结点，等需要再说
+
+	int init_dfs(int p);
+public:
+	Tree(std::vector<int> f, std::vector<long long> d) // 只要一层双亲数组、双亲边的长度和结点数量即可建树，注意根节点的双亲应该是自己
+	:fa(new int*[f.size()]), n(f.size() - 1), lgn(0), dep(new int[f.size()]), maxd(new long long*[f.size()])//lgn先等于0
+	{
+		fa[0] = nullptr;
+		while((1 << lgn) < n)lgn++;
+		for(int i = 1;i <= n;++i)
+		{
+			fa[i] = new int[lgn + 1];
+			maxd[i] = new long long[lgn + 1];
+			fa[i][0] = f[i];
+			dep[i] = 0;
+			maxd[i][0] = d[i];
+		}
+		for(int i = 1;i <= lgn;++i)
+		{
+			for(int j = 1;j <= n;++j)
+			{
+				fa[j][i] = fa[fa[j][i - 1]][i - 1];
+				maxd[j][i] = std::max(maxd[j][i - 1], maxd[fa[j][i - 1]][i - 1]);
+			}
+		}
+		for(int i = 1;i <= n;++i)
+		{
+			init_dfs(i);
+		}
+	}
+	~Tree();
+
+	long long MaxdOnLCA(int a, int b) // 返回树中结点a和结点b之间路径上的最长边（如果先暂时不反悔LCA了，等需要再说）
+	{
+		long long res = 0;
+        if(dep[a] > dep[b])std::swap(a, b);//确保dep[a] <= dep[b];
+        for(int i = lgn;i >= 0 && dep[a] < dep[b];i--)
+        {
+            if(dep[a] <= dep[fa[b][i]])
+            {
+                res = std::max(res, maxd[b][i]);
+                b = fa[b][i];
+            }
+        }
+        if(a == b)return res;
+        for(int i = lgn;i >= 0;i--)
+        {
+            if(fa[a][i] != fa[b][i])
+            {
+                res = std::max(res, std::max(maxd[a][i], maxd[b][i]));
+                a = fa[a][i];
+                b = fa[b][i];
+            }
+        }
+        res = std::max(res, std::max(maxd[a][0], maxd[b][0]));
+        // a = fa[a][0];
+        // b = fa[b][0];
+        return res;
+	}
+};
+
 void Init(const string& command); //接受的string是从文件读入的结果，预处理（如导入图），返回类型暂时为void，可根据需要更改
 
-string task1(const string& command);
-string task2(const string& command);
 string task3(const string& command);
 string task4(const string& command);
 string task5(const string& command);
